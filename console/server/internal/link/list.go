122187c9efb308fbf4b5203d62052b53826dfd63
Kentaro Abe <kentaro@beah.jp>
2024-06-01 03:55:30 +0900

    add: Create API

--- console/server/internal/link/list.go
+++ console/server/internal/link/list.go
@@ -1,8 +1,11 @@
 package link
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
 )
 
 func List(req *gin.Context) {
@@ -14,7 +17,12 @@ func List(req *gin.Context) {
 		return
 	}
 
-	defer cur.Close(ctx)
+	defer func(cur *mongo.Cursor, ctx context.Context) {
+		err := cur.Close(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(cur, ctx)
 	err = cur.All(ctx, &links)
 	if err != nil {
 		req.JSON(500, gin.H{"error": err.Error()})
