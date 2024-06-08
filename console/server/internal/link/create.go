31546c0dfd36fd6637f560e91fa43a0fb86fec9c
Kentaro Abe <kentaro@beah.jp>
2024-06-08 18:52:13 +0900

    add: パス未指定時のランダム文字列作成・ユニーク性保持

--- console/server/internal/link/create.go
+++ console/server/internal/link/create.go
@@ -1,7 +1,10 @@
 package link
 
 import (
+	"crypto/rand"
+	"errors"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -11,6 +14,22 @@ func Create(req *gin.Context) {
 	err := req.BindJSON(&l)
 	if err != nil {
 		req.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	if l.Path == "" {
+		var path string
+		path, err = MakeRandomStr(6)
+		if err != nil {
+			req.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		l.Path = path
+	}
+
+	cols := collection.FindOne(req.Request.Context(), bson.M{"path": l.Path})
+	if cols.Err() == nil {
+		req.JSON(400, gin.H{"error": "path already exists"})
 		return
 	}
 
@@ -25,4 +44,22 @@ func Create(req *gin.Context) {
 	insertedID = r.InsertedID.(primitive.ObjectID)
 
 	req.JSON(201, gin.H{"id": insertedID.Hex()})
+}
+
+func MakeRandomStr(digit uint32) (string, error) {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	// 乱数を生成
+	b := make([]byte, digit)
+	if _, err := rand.Read(b); err != nil {
+		return "", errors.New("unexpected error...")
+	}
+
+	// letters からランダムに取り出して文字列を生成
+	var result string
+	for _, v := range b {
+		// index が letters の長さに収まるように調整
+		result += string(letters[int(v)%len(letters)])
+	}
+	return result, nil
 }
